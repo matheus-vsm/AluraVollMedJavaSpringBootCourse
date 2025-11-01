374e6df8e5283409e7716e3987d2aab3c4746ecd
matheus-vsm <scompinha@hotmail.com>
2025-10-31 22:28:37 -0300

    aula 3,6

---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,22 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable // é usada para marcar uma classe como incorporável dentro de uma entidade JPA.
+// não gera tabela própria no banco.
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String cidade;
+    private String uf;
+    private String complemento;
+    private String numer;
+}
---
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,28 @@
+package med.voll.api.medico;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
+@Getter // gera o metodo get de todos os atributos
+@NoArgsConstructor // gera o construtor padrao
+@AllArgsConstructor // gera o construtor com todos os atributos
+@EqualsAndHashCode(of = "id") // Essa anotação gera automaticamente os métodos equals() e hashCode()
+// para a classe, usando somente o atributo "id" para comparação entre objetos.
+public class Medico {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;;
+    private String nome;
+    private String email;
+    private String crm;
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+    @Embedded // é usado para incluir um objeto dentro de uma entidade, sem criar uma tabela separada no banco
+    // | a tabela Medicos conterá os atributos do endereço
+    private Endereco enderec;
+}
