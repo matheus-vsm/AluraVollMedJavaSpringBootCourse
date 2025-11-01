8f3a23d69e9f1c7ffa4ebcca760e06ab4fde6e00
matheus-vsm <scompinha@hotmail.com>
2025-10-31 23:10:45 -0300

    aula 3.10

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,7 @@
 package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
@@ -20,7 +21,7 @@ public class MedicoController {
     @Transactional // é uma anotação do Spring utilizada para controlar transações no banco de dados.
     //Ela garante que um conjunto de operações (inserir, atualizar, deletar, etc.)
     // seja executado como uma única transação, ou seja: Ou tudo acontece com sucesso, ou nada acontece.
-    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
     }
 }
--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,10 +1,26 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
-public record DadosCadastroMedico(String nome,
-                                  String email,
-                                  String crm,
-                                  Especialidade especialidade,
-                                  DadosEndereco endereco) {
+public record DadosCadastroMedico(
+        @NotBlank // verifica se o campo esta nullo ou vazio
+        String nome,
+        @NotBlank // apenas para strings
+        @Email // veficica se está no formato de email
+        String email,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}") // expressão regular: campo representa um digito
+                                      // numerico 0-9 e tem no min 4 e max 6 carac
+        String crm,
+        @NotNull
+        Especialidade especialidade,
+        @NotNull
+        @Valid // ativa a validação dos atributos do objeto, verificando todas as anotações de
+               // Bean Validation dentro dele.
+        DadosEndereco endereco) {
 }
