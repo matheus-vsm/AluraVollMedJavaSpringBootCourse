dd1190806bdce1f3ca4b0448a3e5beb96187b799
matheus-vsm <scompinha@hotmail.com>
2025-11-02 23:49:19 -0300

    aula 4.6

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -9,6 +9,7 @@ import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,7 +29,8 @@ public class MedicoController {
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> listar(Pageable paginacao) {
+    // caso não seja passado na URL, por padrão, o tamanho das respostas é 10 e ordenado pelo nome
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
         // buscar todos os dados de medico transforma cada elemento do stream | para cada Medico,
         // chama o construtor de Dados, convertendo
         return repository.findAll(paginacao).map(DadosListagemMedico::new);
