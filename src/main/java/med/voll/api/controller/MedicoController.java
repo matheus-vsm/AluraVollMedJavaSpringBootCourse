4d879e3603209fa81c6e84f7c662f0ac6eb95537
matheus-vsm <scompinha@hotmail.com>
2025-11-05 22:54:03 -0300

    aula 1.3

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,24 +22,27 @@ public class MedicoController {
     @Transactional // é uma anotação do Spring utilizada para controlar transações no banco de dados.
     //Ela garante que um conjunto de operações (inserir, atualizar, deletar, etc.)
     // seja executado como uma única transação, ou seja: Ou tudo acontece com sucesso, ou nada acontece.
-    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
-    }
+    } // 201: Requisição Processada e Novo Recurso Criado
 
     @GetMapping
     // caso não seja passado na URL, por padrão, o tamanho das respostas é 10 e ordenado pelo nome
-    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+    public ResponseEntity<Page<DadosListagemMedico>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
         // buscar todos os dados de medico transforma cada elemento do stream | para cada Medico,
         // chama o construtor de Dados, convertendo
-        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+        var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+
+        return ResponseEntity.ok(page); // 200
     }
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
         // jpa rodando em uma transactional já identifica as mudancas e altera automaticamente no banco
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
     }
 
     // deleta o dado do banco
@@ -50,8 +54,10 @@ public class MedicoController {
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void excluir(@PathVariable Long id) {
+    public ResponseEntity excluir(@PathVariable Long id) {
         var medico = repository.getReferenceById(id);
         medico.excluir();
+
+        return ResponseEntity.noContent().build(); // 204: Requisição Processada e Sem Conteúdo
     }
 }
