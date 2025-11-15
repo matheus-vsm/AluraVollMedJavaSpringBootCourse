d5f5514ca5a1ee26e0350ba6c3c9018dcd9e8431
matheus-vsm <scompinha@hotmail.com>
2025-11-15 00:08:59 -0300

    aula 5.6

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,11 +13,16 @@ import java.io.IOException;
 @Component // usado para que o spring carregue um classe/componente genérico
 public class SecurityFilter extends OncePerRequestFilter { // classe para fazer a validação do token, é chamada
     // antes mesmo da requisição chegar no controller, intercepta
+
+    @Autowired
+    private TokenService tokenService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // o envio do token é realizado em um cabeçalho/Header do protocolo HTTP, no caso o Authorization Header
         var tokenJWT = recuperarToken(request);
-        System.out.println(tokenJWT);
+        var subject = tokenService.getSubject(tokenJWT);
+        System.out.println(subject);
         // manda seguir o fluxo de requisição / necessário para chmar os proximos filtros na aplicaçõ /
         // APENAS PARA SUCESSO, NAO RODAR ISSO QUANDO FOR PARA INTERROMPER A EXECUÇAÕ
         filterChain.doFilter(request, response); // filterChain = representa a cadeia de filtros da aplicação
