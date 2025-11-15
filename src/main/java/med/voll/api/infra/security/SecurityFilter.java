39da1909db93799a913d00aa1f70666ec6ed8546
matheus-vsm <scompinha@hotmail.com>
2025-11-14 23:58:25 -0300

    aula 5.5

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -1,6 +1,5 @@
 package med.voll.api.infra.security;
 
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,8 +14,19 @@ public class SecurityFilter extends OncePerRequestFilter { // classe para fazer 
     // antes mesmo da requisição chegar no controller, intercepta
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // o envio do token é realizado em um cabeçalho/Header do protocolo HTTP, no caso o Authorization Header
+        var tokenJWT = recuperarToken(request);
+        System.out.println(tokenJWT);
         // manda seguir o fluxo de requisição / necessário para chmar os proximos filtros na aplicaçõ /
         // APENAS PARA SUCESSO, NAO RODAR ISSO QUANDO FOR PARA INTERROMPER A EXECUÇAÕ
         filterChain.doFilter(request, response); // filterChain = representa a cadeia de filtros da aplicação
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization"); // define qual o cabeçalho ele vai resgatar
+        if (authorizationHeader == null) {
+            throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
+        }
+        return authorizationHeader.replace("Bearer", ""); // tira o prefixo Bearer
     }
 }
