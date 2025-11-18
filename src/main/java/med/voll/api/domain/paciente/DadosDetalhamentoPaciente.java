52419b759422e7bf6b0d05566f536d81c7216387
matheus-vsm <scompinha@hotmail.com>
2025-11-17 23:48:50 -0300

    criacao da entidade Paciente. metodos cadastrar listar pacientes feito. continuar com atualização e exclusão

---
+++ src/main/java/med/voll/api/domain/paciente/DadosDetalhamentoPaciente.java
@@ -0,0 +1,18 @@
+package med.voll.api.domain.paciente;
+
+import jakarta.persistence.Embedded;
+import med.voll.api.domain.endereco.Endereco;
+
+public record DadosDetalhamentoPaciente(
+        Long id,
+        String nome,
+        String email,
+        String telefone,
+        String cpf,
+        Endereco endereco
+) {
+    public DadosDetalhamentoPaciente(Paciente paciente) {
+        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getTelefone(),
+                paciente.getCpf(), paciente.getEndereco());
+    }
+}
