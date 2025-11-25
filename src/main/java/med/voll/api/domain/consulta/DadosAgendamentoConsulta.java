483d824d168ff3b1ff927257c761e4a5bcf7c3ec
matheus-vsm <scompinha@hotmail.com>
2025-11-24 21:44:14 -0300

    aula 1.9

--- src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
@@ -2,15 +2,20 @@ package med.voll.api.domain.consulta;
 
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
+import med.voll.api.domain.medico.Especialidade;
 
 import java.time.LocalDateTime;
 
 public record DadosAgendamentoConsulta(
         Long idMedico,
+
         @NotNull
         Long idPaciente,
+
         @NotNull
         @Future // indica que a data deve ser no futuro
-        LocalDateTime data
+        LocalDateTime data,
+
+        Especialidade especialidade
 ) {
 }
